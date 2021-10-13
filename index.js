4be2126f92fa345ce671fae1efb5e7d3e9abcc43
jvpink200 <jvillarr20@gmail.com>
2021-10-12 20:23:02 -0700

    git status

--- index.js
+++ index.js
@@ -1,22 +1,40 @@
-let firstCard = 17;
-let secondCard = 6;
-
+let firstCard = 13;
+let secondCard = 2;
 let sum = firstCard + secondCard;
+let hasBlackJack = false;
+let isAlive = true;
+let message = '';
 
-if (sum < 21) {
-    console.log('Do you want to draw a new card?');
-} else if (sum === 21) {
-    console.log('Wohoo! You won!')
-} else {
-    console.log("You're out of the game!")
+let msgEl = document.getElementById("message-el");
+
+
+function startGame() {
+  if (sum <= 20) {
+    message = "Do you want to draw a new card? 🙂";
+  } else if (sum === 21) {
+    hasBlackJack = true;
+    message = "Wohoo! You won! 🥳";
+  } else {
+    isAlive = false;
+    message = "You're out of the game! 😭";
+  }
+  msgEl.textContent = message;
 }
 
+
+
+
+
+//console.log(hasBlackJack);
+//console.log(isAlive);
 //check age
 
-let personAge = 21;
+// let personAge = 21;
 
-if(personAge < 21) {
-    console.log('YOu can not enter the club!');
-} else{
-    console.log('Welcome!');
-}
+// if(personAge < 21) {
+//     console.log('YOu can not enter the club!');
+// } else{
+//     console.log('Welcome!');
+// }
+
+  
